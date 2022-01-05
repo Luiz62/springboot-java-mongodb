7e1b84a3a77cfa216c6846cb019b2a46a45dd397
Luiz62 <luiz.nascimento@foton.la>
2022-01-04 21:23:19 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -0,0 +1,58 @@
+package com.example.workshopmongo.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.dto.PostDTO;
+import com.example.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<PostDTO>> findAll() {
+		return ResponseEntity.ok().body(postService.findAll().stream().map(x -> new PostDTO(x)).collect(Collectors.toList()));
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
+		return ResponseEntity.ok().body(new PostDTO(postService.findById(id)));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody PostDTO objDto) {
+		Post obj = postService.fromDTO(objDto);
+		obj = postService.insert(obj);	
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		postService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody PostDTO objDto) {
+		Post obj = postService.fromDTO(objDto);
+		obj.setId(id);
+		obj = postService.update(obj);	
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,54 @@
+package com.example.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.dto.PostDTO;
+import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository userRepository;
+	
+	public List<Post> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public Post findById(String id){
+		Optional<Post> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Post insert(Post obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	public Post update(Post obj) {
+		Post newObj = findById(obj.getId());
+		UpdateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+ 	
+	private void UpdateData(Post newObj, Post obj) {
+		newObj.setDate(obj.getDate());
+		newObj.setTitle(obj.getTitle());
+		newObj.setBody(obj.getBody());
+	}
+
+	public Post fromDTO(PostDTO obj) {
+		return new Post(obj.getId(), obj.getDate(), obj.getTitle(), obj.getBody());
+	}
+	
+}

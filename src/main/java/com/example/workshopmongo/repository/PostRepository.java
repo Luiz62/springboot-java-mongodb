e6f1cb7a5cec6ada87295683bdbf94598281a606
Luiz62 <85712429+Luiz62@users.noreply.github.com>
2022-01-06 07:45:54 -0300

    Update PostRepository.java

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -9,6 +9,10 @@ import com.example.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
 
